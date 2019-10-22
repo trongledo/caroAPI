2d34b4819aa9bdcf44745b179b052f6b5f846fc9
Trong Ledo <trong.ledo136@gmail.com>
2019-10-22 14:01:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
   next();
 });
 
